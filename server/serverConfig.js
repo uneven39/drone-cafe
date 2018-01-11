174e322abc946b68d9955f0ecb74fab698b8b2db
uneven39 <unwar39@gmail.com>
2018-01-11 14:23:14 +0200

    init commit

--- server/serverConfig.js
+++ server/serverConfig.js
@@ -3,6 +3,7 @@ module.exports = {
 		port: 3000
 	},
 	db: {
-		url: 'mongodb://localhost:27017/droneCafe'
+		// url: 'mongodb://localhost:27017/droneCafe'
+		url: 'mongodb://test:test@ds251807.mlab.com:51807/heroku_10hvscpp'
 	}
 };
